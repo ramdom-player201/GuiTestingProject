25902a8d9213e660a5d29a38a73b5e133200fb08
ramdom_player201 <davypj06@gmail.com>
2025-12-10 11:55:10 +0000

    Trial #ifdef features to see if it works on linux/macos. (eg cut window's specific code if _WIN64)

--- src/Core/Services/CommandConsole.cpp
+++ src/Core/Services/CommandConsole.cpp
@@ -29,14 +29,21 @@ void CommandConsole::parseCommand()
 	parseCommand(input);
 }
 
+#ifdef _WIN64
 #include <Windows.h>
+#endif
 
+// windows only rn, linux support is for when I have a linux device to test with
 void CommandConsole::focusConsole()
 {
 	std::cout << ConsoleColours::getColourCode(AnsiColours::DEFAULT) << "WIP: CommandConsole -> Console may not work correctly on linux, additional testing recommended\n";
+#ifdef _WIN64
 	HWND consoleHandle = GetConsoleWindow();
 	if (consoleHandle) {
 		ShowWindow(consoleHandle, SW_RESTORE);
 		SetForegroundWindow(consoleHandle);
 	}
+#else
+	std::cout << ConsoleColours::getColourCode(AnsiColours::Red) << "WIP: CommandConsole -> This feature is currently disabled for Mac and Linux\n";
+#endif
 }
