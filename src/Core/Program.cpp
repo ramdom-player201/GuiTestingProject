09484dc40552a6c422c313743431f5a37e907a66
ramdom_player201 <davypj@gmail.com>
2026-01-14 22:48:12 +0000

    Finish removing old Services.h code.

--- src/Core/Program.cpp
+++ src/Core/Program.cpp
@@ -6,7 +6,8 @@
 
 void Program::Run()
 {
-	std::cout << ConsoleColours::getColourCode(AnsiColours::GREEN) << "Starting "
+	std::cout
+		<< ConsoleColours::getColourCode(AnsiColours::GREEN) << "Starting "
 		<< ConsoleColours::getColourCode(AnsiColours::CYAN) << "program\n";
 	LogService::Log(
 		LogType::TRACE,
@@ -17,10 +18,10 @@ void Program::Run()
 
 	VulkanHandler::Initialise();
 
-	Services::initialise();
+	//Services::initialise();
 	//auto windowManager = dynamic_cast<WindowManager*>(Services::getService(ServiceType::WindowManager));
 	//auto windowManager2 = dynamic_cast<WindowManager*>(Services::getService(ServiceType::WindowManager));
-	auto commandConsole = dynamic_cast<CommandConsole*>(Services::getService(ServiceType::CommandConsole));
+	//auto commandConsole = dynamic_cast<CommandConsole*>(Services::getService(ServiceType::CommandConsole));
 	//auto objectManager = dynamic_cast<ObjectManager*>(Services::getService(ServiceType::ObjectManager));
 
 	WindowManager::debugMode = true;
@@ -36,9 +37,9 @@ void Program::Run()
 	while (WindowManager::countWindows() > 0) {
 		// program should get call console to get commands
 		bool commandLinePause = WindowManager::updateWindows();
-		if (commandLinePause) {
-			commandConsole->parseCommand();
-		}
+		//if (commandLinePause) {
+		//	commandConsole->parseCommand();
+		//}
 	}
 }
 
--- src/Core/Services.h
+++ src/Core/Services.h
@@ -11,53 +11,53 @@
 #include "Services/LogService.h"
 #include "Services/VulkanHandler.h"
 
-enum class ServiceType {
-	CommandConsole,
-	//WindowManager,
-	ObjectManager
-};
-
-class Services {
-private:
-	// define instance list and mtx
-	inline static std::mutex mtx; // for multi-thread locking
-	inline static std::map<ServiceType, std::unique_ptr<IService>> instances;
-
-	Services() = delete; // prevent instanciation
-
-	static IService* createService(ServiceType service) {
-		switch (service) {
-		case ServiceType::CommandConsole: {
-			return new CommandConsole();
-		}
-		//case ServiceType::WindowManager: {
-		//	return new WindowManager();
-		//}
-		case ServiceType::ObjectManager: {
-			return new ObjectManager();
-		}
-		default:
-			throw std::invalid_argument("Unknown service type");
-		}
-	}
-
-public:
-	static IService* getService(ServiceType type) {
-		auto& instance = instances[type];
-
-		// double-checked locking (for thread-safety or something)
-		if (!instance) {
-			std::lock_guard<std::mutex> lock(mtx); // lock is removed when lock exits scope
-			if (!instance) {
-				instance = std::unique_ptr<IService>(createService(type));
-			}
-		}
-		return instance.get();
-	}
-
-	static void initialise() {
-		// register services with their implementations
-		instances[ServiceType::CommandConsole] = nullptr;
-		//instances[ServiceType::WindowManager] = nullptr;
-	}
-};
+//enum class ServiceType {
+//	CommandConsole,
+//	//WindowManager,
+//	ObjectManager
+//};
+//
+//class Services {
+//private:
+//	// define instance list and mtx
+//	inline static std::mutex mtx; // for multi-thread locking
+//	inline static std::map<ServiceType, std::unique_ptr<IService>> instances;
+//
+//	Services() = delete; // prevent instanciation
+//
+//	static IService* createService(ServiceType service) {
+//		switch (service) {
+//		case ServiceType::CommandConsole: {
+//			return new CommandConsole();
+//		}
+//		//case ServiceType::WindowManager: {
+//		//	return new WindowManager();
+//		//}
+//		case ServiceType::ObjectManager: {
+//			return new ObjectManager();
+//		}
+//		default:
+//			throw std::invalid_argument("Unknown service type");
+//		}
+//	}
+//
+//public:
+//	static IService* getService(ServiceType type) {
+//		auto& instance = instances[type];
+//
+//		// double-checked locking (for thread-safety or something)
+//		if (!instance) {
+//			std::lock_guard<std::mutex> lock(mtx); // lock is removed when lock exits scope
+//			if (!instance) {
+//				instance = std::unique_ptr<IService>(createService(type));
+//			}
+//		}
+//		return instance.get();
+//	}
+//
+//	static void initialise() {
+//		// register services with their implementations
+//		instances[ServiceType::CommandConsole] = nullptr;
+//		//instances[ServiceType::WindowManager] = nullptr;
+//	}
+//};
